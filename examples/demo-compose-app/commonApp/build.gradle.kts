87db40d45cf8a5d88328260975bd2bd749f6ae34
Konstantin Pavlov <1517853+kpavlov@users.noreply.github.com>
2025-11-13 16:16:42 +0200

    Fix java version across gradle projects. Use npm

--- examples/demo-compose-app/commonApp/build.gradle.kts
+++ examples/demo-compose-app/commonApp/build.gradle.kts
@@ -1,4 +1,7 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
 import org.jetbrains.compose.reload.gradle.ComposeHotRun
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
@@ -10,7 +13,7 @@ plugins {
 }
 
 kotlin {
-    jvmToolchain(libs.versions.jdkVersion.get().toInt())
+    jvmToolchain(libs.versions.javaVersion.get().toInt())
 
     android {
         namespace = "com.jetbrains.example.koog.share.ui"
